255b7dce903882937298390856d45e9ecd377efb
binduk68 <bk1044@wildcats.unh.edu>
2019-02-21 00:33:21 -0500

    Update peerserver.js

--- peerserver.js
+++ peerserver.js
@@ -9,9 +9,6 @@ var PeerServer = require('peer').PeerServer;
 
 var server = PeerServer({
     port: 9000,
-    path: '/peerjs',
-    ssl: {
-       key: fs.readFileSync('./../certificates/key.pem', 'utf8'),
-       cert: fs.readFileSync('./../certificates/cert.pem', 'utf8')
-     }
+    path: '/peerjs'
+    
 });
